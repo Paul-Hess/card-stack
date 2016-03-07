75eade0d979652dbf43cf8a1acddae4e0fbfd693
Paul-Hess <pauljacksonhess@gmail.com>
2016-03-06 23:01:10 -0800

    smooth transition of card animations

--- js/main.js
+++ js/main.js
@@ -2,8 +2,6 @@ $(document).ready(function() {
 	$('.phyllum').each(function() {
 		$(this).click(function() {
 			$(this).toggleClass('move-up');
-			$currentDiv = $(this);
-			$('.phyllum').not('.move-up').toggleClass('move-out');
 		});
 	});
 });
